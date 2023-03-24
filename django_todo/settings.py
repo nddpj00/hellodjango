7ec02c40ab6cc783c23c566dfea777564084e148
nddpj00 <danieljones625@btinternet.com>
2023-03-24 13:54:23 +0000

    set up environment variables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'lionel_richtea')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nddpj00-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
